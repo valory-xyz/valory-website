6509668bb98431cb9dd9f3980fb6be2c4a1bb332
atepem <57289722+atepem@users.noreply.github.com>
2024-12-19 01:24:51 +0000

    chore: code fixes and img quality

---
+++ data/investors.tsx
@@ -0,0 +1,8 @@
+export const investors = [
+  'true-ventures-logo.png',
+  'signature-ventures-logo.png',
+  'semantic-logo.png',
+  'prime-block-logo.png',
+  'proof-logo.png',
+  'atka-logo.png',
+];

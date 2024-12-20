b275e94b6de3412a43b5501d27f2cbed1b725d03
Atatakai <densatsu@mail.ru>
2024-12-20 13:13:38 +0400

    fix: pdf render

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,8 +1,45 @@
-// pages/post/[id].tsx
-
 import { Layout } from 'components/Layout';
 import { Meta } from 'components/Meta';
 import { useRouter } from 'next/router';
+
+import React, { useState, useRef } from 'react';
+import { usePdf } from '@mikecousins/react-pdf';
+
+const PdfContent = ({ id }: { id: string | string[] }) => {
+  const [page, setPage] = useState(1);
+  const canvasRef = useRef(null);
+
+  const { pdfDocument } = usePdf({
+    file: `/news-posts/${id}.pdf`,
+    page,
+    canvasRef,
+  });
+
+  return (
+    <div className="max-w-[900px] w-full p-[50px] border border-gray-300">
+      {!pdfDocument && <span>Loading...</span>}
+      <canvas ref={canvasRef} className="max-w-[800px]" />
+      {pdfDocument && pdfDocument.numPages && (
+        <div className="flex justify-end gap-4 mt-4">
+          <button
+            className="btn btn-sm"
+            disabled={page === 1}
+            onClick={() => setPage((prevPage) => prevPage - 1)}
+          >
+            Previous
+          </button>
+          <button
+            className="btn btn-sm"
+            disabled={page >= pdfDocument.numPages}
+            onClick={() => setPage((prevPage) => prevPage + 1)}
+          >
+            Next
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 const Post = () => {
   const router = useRouter();
@@ -15,10 +52,8 @@ const Post = () => {
   return (
     <Layout>
       <Meta />
-      <section className="pt-20">
-        <h1>Post {id}</h1>
-
-        <iframe src={`/news-posts/${id}.pdf`} width="100%" height="600px" />
+      <section className="pt-40 p-20 flex justify-center">
+        <PdfContent id={id} />
       </section>
     </Layout>
   );
